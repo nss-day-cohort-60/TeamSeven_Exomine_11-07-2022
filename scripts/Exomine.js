cd79c2afd927a7e149ae7f5f5bae712c855107be
James Linkous <james.william.linkous@gmail.com>
2022-11-08 10:02:09 -0600

    html

--- scripts/Exomine.js
+++ scripts/Exomine.js
@@ -1,1 +1,14 @@
 //Organizing html string functions
+import { Governors } from "./Governors.js"
+import { Facilities } from "./Facilities.js"
+import { Cart } from "./Cart.js"
+
+export const Exomine = () => {
+    const html=`
+    <title id="title">Exomine</title>
+    ${Governors()}
+    ${Facilities()}
+    ${Cart()}
+    `
+    return html
+}
