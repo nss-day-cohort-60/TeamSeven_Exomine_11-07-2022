731ab6cd9037f42cb2ab9e72dda8a31e8cc1f196
James Linkous <james.william.linkous@gmail.com>
2022-11-09 10:45:10 -0600

    message

--- src/scripts/Exomine.js
+++ src/scripts/Exomine.js
@@ -1,9 +1,7 @@
 //Organizing html string functions
 import { Governors } from "./Governors.js"
 import { Facilities } from "./Facilities.js"
-import { setGovernorsChoice } from "./dataAccess.js"
 
-const mainContainer = document.querySelector("#mainContainer")
 
 export const Exomine = () => {
     const html=`
