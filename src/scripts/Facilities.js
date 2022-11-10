a7b4d3e59639c4702658c0ab73af7d4ad08ae298
lohanson33@gmail.com <lohanson33@gmail.com>
2022-11-10 09:52:42 -0600

    Updated database

--- src/scripts/Facilities.js
+++ src/scripts/Facilities.js
@@ -1,7 +1,7 @@
 //<select> of facility names IF governor drop down<select> has changed.
 //display available minerals as radio buttons when "change" eventListener happens.
 //change events for radio buttons that adds 1 ton to transitory state (cart)
-import { getFacilities, getTransient, setFacilitiesChoice, setPurchaseChoice } from "./dataAccess.js"
+import { getFacilities, getTransient, setFacilitiesChoice, setPurchaseChoice, getFacilityMineralLog } from "./dataAccess.js"
 
 
 export const Facilities = () => {
@@ -9,16 +9,16 @@ export const Facilities = () => {
     const transient = getTransient()
     let html = ""
 
-    if(transient.governorsChoice){
-        html+=`<label for="chooseFacility">Choose a Facility</label>
+    if (transient.governorsChoice) {
+        html += `<label for="chooseFacility">Choose a Facility</label>
         <select id="facility" name="chooseFacility" value="1">
         <option value="0">Choose a Facility</option>
-        ${facilities.map(facility=>{
+        ${facilities.map(facility => {
             return `<option name="chooseFacility" value="${facility.name}">${facility.name}</option>`
         })}
         </select>`
-    }else {
-        html+=`<label for="chooseFacility">Choose a Facility</label>
+    } else {
+        html += `<label for="chooseFacility">Choose a Facility</label>
         <select id="facility" name="chooseFacility" value="1">
         <option value="0">First, Choose a Governor</option>
         </select>`
@@ -29,38 +29,44 @@ export const Facilities = () => {
 
 
 export const FacilityMinerals = () => {
+
     const facilities = getFacilities()
+    const facilityLog = getFacilityMineralLog()
     const transient = getTransient()
+  
+
     let html = ""
-    if (transient.facilitiesChoice){
-        for (const facility of facilities){
-            if(facility.name === transient.facilitiesChoice){
-                html += `
+    if (transient.facilitiesChoice) {
+        for (const facility of facilities) {
+            for (const mineral of facilityLog) {
+                if (facility.name === transient.facilitiesChoice && facility.id === mineral.mineral.id) {
+                    html += `
                 <legend>Minerals Available at ${facility.name}</legend>
                 
                 <div class="mineralRadio">
                     <input type="radio" id="iron" name="minerals">
-                    <label for="${facility.iron}">${facility.iron} tons of Iron</label>
+                    <label for="${facility.id}">${mineral.stock} tons of Iron</label>
                     <input type="radio" id="transparentAluminum" name="minerals">
-                    <label for="${facility.transparentAluminum}">${facility.transparentAluminum} tons of Transparent Aluminum</label>
+                    <label for="${facility.id}">${mineral.stock} tons of Transparent Aluminum</label>
                     <input type="radio" id="chromium" name="minerals">
-                    <label for="${facility.chromium}">${facility.chromium} tons of Chromium</label>
+                    <label for="${facility.id}">${mineral.stock} tons of Chromium</label>
                     <input type="radio" id="beryllium" name="minerals">
-                    <label for="${facility.beryllium}">${facility.beryllium} tons of Beryllium</label>
+                    <label for="${facility.id}">${mineral.stock} tons of Beryllium</label>
                     <input type="radio" id="benadryl" name="minerals">
-                    <label for="${facility.benadryl}">${facility.benadryl} tons of Benadryl</label>
+                    <label for="${facility.id}">${mineral.stock} tons of Benadryl</label>
                 </div>
                `
+                }
             }
         }
-    }else{
+    } else {
         html += `<legend>Facility Minerals</legend>`
     }
     return html
 }
 
 mainContainer.addEventListener("change", (event) => {
-    if(event.target.id === "facility") {
+    if (event.target.id === "facility") {
         setFacilitiesChoice(event.target.value)
         // renderColony()
         mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
@@ -69,7 +75,7 @@ mainContainer.addEventListener("change", (event) => {
 })
 
 mainContainer.addEventListener("change", (event) => {
-    if(event.target.name === "minerals") {
+    if (event.target.name === "minerals") {
         setPurchaseChoice(event.target.id)
         // renderColony()
         mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
